a95c3959963195d6931030f99a8c1ccf1968bdd3
Kyoung-min Kang <kyoungmin.kang@gmail.com>
2023-09-18 19:14:56 +0900

    Add files via upload

---
+++ app/chat/layout.tsx
@@ -0,0 +1,23 @@
+import { ChatMenu } from "@/features/chat/chat-menu/chat-menu";
+import { MainMenu } from "@/features/menu/menu";
+import { AI_NAME } from "@/features/theme/customise";
+
+export const metadata = {
+  title: AI_NAME,
+  description: AI_NAME,
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <MainMenu />
+      <div className="flex-1 flex rounded-md overflow-hidden bg-card/70">
+        <ChatMenu /> {children}
+      </div>
+    </>
+  );
+}
